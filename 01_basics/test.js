c561fd8539b15b69dd87680099f32d4872f908ac
jayeshsonavane2025 <jayeshsonavane2025@gmail.com>
2023-09-17 19:49:10 +0530

    conversion  datatypes

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,3 @@
+
+console.log("Radhe Radhe")
 console.log("Radhe Radhe")
---
+++ 01_basics/var.js
@@ -0,0 +1,19 @@
+let naam = "Hari"
+console.log(naam); 
+console.log(typeof (naam));
+
+naam = Number(naam);
+
+console.log(naam)
+
+let marks = null
+
+console.log(marks)
+
+let num = 33
+
+console.log(String(num))
+
+num=String(num)
+
+console.log(typeof num)
