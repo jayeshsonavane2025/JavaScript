a01d67d06a4ada840443289d00029e63c295db3e
jayeshsonavane2025 <jayeshsonavane2025@gmail.com>
2023-09-19 18:43:04 +0530

    create op.js

---
+++ 01_basics/op.js
@@ -0,0 +1,15 @@
+let value = 3
+let negvalue = -value
+console.log(negvalue)
+console.log(2**3)
+console.log(2 % 3)
+console.log(2 + 3)
+
+let naam1 = "krishna"
+let naam2 = " radhe"
+
+console.log(naam2 + " " + naam1)
+
+console.log(2 % "3")
+
+console.log(2 % 3 + "3")
