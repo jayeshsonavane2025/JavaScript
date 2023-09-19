4d803bcacc09bec3794ac5e901ea2b3f87608e7a
jayeshsonavane2025 <jayeshsonavane2025@gmail.com>
2023-09-19 18:52:10 +0530

    learning operators in js

--- 01_basics/op.js
+++ 01_basics/op.js
@@ -13,3 +13,23 @@ console.log(naam2 + " " + naam1)
 console.log(2 % "3")
 
 console.log(2 % 3 + "3")
+
+console.log(true+ 1)
+
+console.log(typeof naam1)
+
+console.log(typeof " ")
+
+console.log(2 ** 3)
+
+let n1=n2=n3 = 2
+
+console.log(n1)
+
+console.log(n2 + n3)
+
+let count = 1
+
+console.log(count++)
+
+console.log( count)
