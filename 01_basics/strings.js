8bd7440b87158a2552b1031854bc6e3e19d14dd4
jayeshsonavane2025 <jayeshsonavane2025@gmail.com>
2023-09-29 15:19:46 +0530

    Update strings.js

--- 01_basics/strings.js
+++ 01_basics/strings.js
@@ -1,7 +1,17 @@
-const name1 = "krishna"
+const name1 = "   k ri s hna  "
 const count = 1
 
 console.log(name1[0])
 
 console.log(name1.charAt(3))
 console.log(name1.indexOf("ri"))
+
+
+console.log(name1.substring(0, 3))
+console.log(name1.substring(0))
+
+console.log(name1.slice(-10, 3))
+
+console.log(name1.trim())
+
+console.log(name1.split(" "))
