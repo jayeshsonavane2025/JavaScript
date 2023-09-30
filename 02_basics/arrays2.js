c7167b33a30828ad5cbe02ef6b71a7cf3118f0e1
jayeshsonavane2025 <jayeshsonavane2025@gmail.com>
2023-09-30 16:44:39 +0530

    Create arrays2.js

---
+++ 02_basics/arrays2.js
@@ -0,0 +1,11 @@
+arr = [1, 2, 3, 4, 5, 6, 7, 8]
+arr1=[9,10,1,1,2,12,133]
+//console.log(arr)
+// console.log("Push")
+// console.log(arr.push(arr1))
+// console.log("After push")
+// console.log(arr)
+// console.log("Concat")
+// console.log(arr.concat(arr1))
+
+console.log(...arr)
