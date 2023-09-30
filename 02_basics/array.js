1889fd627fca1f1f013ef0fb393f4af4d4ce8cb5
jayeshsonavane2025 <jayeshsonavane2025@gmail.com>
2023-09-30 14:42:45 +0530

    arrays in js

---
+++ 02_basics/array.js
@@ -0,0 +1,38 @@
+//array
+
+let arr = ["Radhe", "Krishna", "Balram", "RadhaVallabh", 1, 2, 3, 4, 1.5, true, ["krishna", "mohan"]]
+
+//console.log(arr)
+
+//console.log(arr[10])
+
+//Arrays method
+
+// arr.push(5)
+// arr.push(11)
+
+
+
+//console.log(arr)
+
+// arr.pop();
+
+// console.log(arr)
+
+
+// console.log(arr.includes(3))
+
+// console.log(arr.indexOf(1))
+
+// console.log(arr.join())
+
+
+let a = [1, 2, 3, 4, 5, 6, 7]
+
+console.log(a.slice(1, 3))
+
+console.log(a)
+
+console.log(a.splice(1, 3))
+
+console.log(a)
