bf89e7aefe1ee64596922195ed4414f038ce8303
jayeshsonavane2025 <jayeshsonavane2025@gmail.com>
2024-04-07 12:25:43 +0530

    Array And Objects

--- 03_Array/array.js
+++ 03_Array/array.js
@@ -1,1 +1,21 @@
 console.log("Radhe radhe Bola kya")
+
+const arr = ["Premanand", "Bhaiji", 'Ramsuykhdasji', 35];
+console.log(arr)
+
+arr[1] = "Potdarji"
+
+console.log(arr)
+
+console.log(Number.MAX_VALUE)
+
+console.log(arr.push(1))
+
+console.log(arr.slice(0, 2))
+
+console.log(arr)
+
+console.log(arr.splice(0, 1));
+
+console.log(arr)
+
---
+++ 03_Array/object.js
@@ -0,0 +1,22 @@
+
+const sym=Symbol("Mercy")
+
+const myObj = {
+    name: "Krishna",
+    nature: "ParamKripalu",
+    work: function (yname) {
+        return (`You are under my Kripa ${yname} yes I am your  ${this.name}`)
+    },
+    [sym]:"Satsang"
+    
+}
+console.log(myObj)
+console.log(myObj[sym])
+console.log(myObj.name)
+console.log(myObj.work("Jayesh"))
+
+
+
+
+
+
