6cbc574044f5355f94080c8d0c88f38c63a8ba2f
jayeshsonavane2025 <jayeshsonavane2025@gmail.com>
2024-04-07 05:38:52 +0530

    Update new.js

--- 01_basics/new.js
+++ 01_basics/new.js
@@ -2,4 +2,16 @@ godName="Krishna"
 godDham="Vrindavan"
 godLoveOne="Premanand Ji Maharaj"
 
-console.table([godName,godDham,godLoveOne])
+//console.table([godName, godDham, godLoveOne])
+
+//console.log(typeof "Krishna")
+//console.error(typeof me)
+//let var const
+//let has local scope
+//var has global
+//const is final simply
+
+console.log("2" == 2)
+console.log("2" === 2)
+
+
