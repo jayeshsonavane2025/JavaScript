c2b91c324b866d4693a466df6404b8adc65e2609
jayeshsonavane2025 <jayeshsonavane2025@gmail.com>
2024-04-07 11:18:38 +0530

    Learn array,date in JS

--- 01_basics/new.js
+++ 01_basics/new.js
@@ -11,7 +11,22 @@ godLoveOne="Premanand Ji Maharaj"
 //var has global
 //const is final simply
 
-console.log("2" == 2)
-console.log("2" === 2)
+// console.log("2" == 2)
+// console.log("2" === 2)
+//  a=new String("Radha")
+// console.log(a.length);
+
+// console.log(Math.floor(Math.random()*(20-10+1)+10))
+
+
+const dateTime = new Date(2011,11,11);
+console.log(dateTime)
+console.log(dateTime.toString())
+console.log(dateTime.toLocaleString())
+
+console.log()
+
+console.log(dateTime.getHours())
+
 
 
---
+++ 03_Array/tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+console.log("Radhe radhe Bola kya")
